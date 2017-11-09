92b2369e54152d499e0e8ce1104ee4bd2ff1fdad
skypto <awuahkwabena@gmail.com>
2017-11-09 23:16:57 +0100

    stripe integration

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # crowdfunding101
-A crowdfunding app for making targeted contributions. This is my first attempt and work in progress. Updates will be added over time. Feel free to add your contributions.
+A crowdfunding app for making targeted contributions. This is my first attempt working with strip API integration.
+
+USE CASE:
+- A basic page provides a link to user to make contribution towards a particular campaign.
+- First redirection: User is prompted to enter credit card detail which gets validated and processed by Stripe API.
+- Second redirection: User is redirected to a new page after successful transaction.
 
 
 - [View Demo](#crowdfunding101)
 
 Technologies used: 
 * Node.js
+* Express and EJS
 * MongoDB
-* Balanced Payments
+* Stripe Payments
 
+# V2.0.0
+* Integrate Stripe API
+* Add final redirect page 
 
 
 # V1.0.0
--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var bodyParser = require("body-parser");
 
 
 app.use(bodyParser.urlencoded({extended:true}));//include body parser for form data
+// app.use(bodyParser.json());
 app.set("view engine","ejs"); //set view engine to remove ejs extension(store all your ejs files here)
 app.use(express.static(__dirname + "/public"));//serve static files egCSS files
+
+var key =require("./key.js")
+//setup payment api details
+var stripe = require("stripe")(key);//API key
 
 
 //Define root route
@@ -13,12 +18,35 @@ app.get("/",function(req,res){
     res.render("index");
 });
 
-//Define funding route
+
+//Define funding route.
 app.get("/fund",function(req,res){
-    res.render("fund");
+    res.sendFile(__dirname +"/views/fund.html");   
 });
 
+//Process the funding
+app.post("/fund",function(req,res){
+    //get the strip token(generated when the form is submitted)
+    var token = req.body.stripeToken;
+    var chargeAmount = req.body.chargeAmount;
+    console.log(token +' '+ chargeAmount)
+    //below is done to prevent man-in-the middle attack (extra verification)
+    var charge = stripe.charges.create({
+        amount: chargeAmount,
+        currency: "eur",
+        source: token
+    },function(err, charge){
+        if (err && err.type === "StripeCardError"){
+                console.log("Your card was declined");
+        }else{
+            res.send("Payment Completed");
+        }
+    });
+    
+
+}); 
 
 app.listen(3000,function(req,res){
     console.log("Server started. Listening on port 3000")
 });
+
